a35785e30cfafad3173ae280872b5ddc9e033358
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2023-06-10 08:29:28 +0300

    x

--- python/main.py
+++ python/main.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, Building python app.....'
-
-if __name__ == '__main__':
-    app.run()
+print('Hello, Building python app.....')
