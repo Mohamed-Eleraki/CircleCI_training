4b61334ff989d6713a6270997e3ab68f43cb3ca8
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2023-06-11 10:29:12 +0300

    x

--- python/main_scan.py
+++ python/main_scan.py
@@ -1,1 +1,17 @@
+import requests
+
 print('Scan python app.....')
+print('Hello, Building python app.....')
+
+#3
+try:
+    response = requests.get('http://google.com')  # get the website code status result
+    if response.status_code == 200:  # if Okay print the below statement
+        print('Application is running successfully!')
+    else:  # if not Send an email
+        print('Application down, Fix it!')
+
+
+except Exception as ex:  # catch the err in ex
+    print(f'connection error happened {ex}')  # print out the error
+    
