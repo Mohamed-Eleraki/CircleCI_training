ccedce13f56157f94727a7c14677636d375ee499
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2023-06-12 10:25:27 +0300

    x

--- python/main.py
+++ python/main.py
@@ -1,15 +1,15 @@
 import requests
 
 
-print('Hello, Building python app.....')
+print('Hello, From Building python app.....')
 
 #3
 try:
     response = requests.get('http://google.com')  # get the website code status result
     if response.status_code == 200:  # if Okay print the below statement
-        print('Application is running successfully!')
+        print('Website is running successfully!')
     else:  # if not Send an email
-        print('Application down, Fix it!')
+        print('Website down, Fix it!')
 
 
 except Exception as ex:  # catch the err in ex
